b53a1ef8dae76e30b31b506ce79d74287fc69c70
danieldanter <daniel.danter@gmx.at>
2021-10-06 15:08:48 +0200

    only bell and some style is missing

--- app.js
+++ app.js
@@ -37,7 +37,10 @@ let trafficData = {
     datasets: [{
     data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
     2500],
-    backgroundColor: 'rgba(116, 119, 191, .3)',
+    //backgroundColor: 'rgba(116, 119, 191, .3)',
+    backgroundColor: 'rgba(65, 105, 225, .4)',
+    borderColor: 'rgba(65, 105, 225, .4)',
+    fill:true,
     borderWidth: 1,
     }]
 };
@@ -64,4 +67,120 @@ let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
     data: trafficData,
     options: trafficOptions
+    });
+
+// ********************** Bar Chart ***************************** 
+
+
+
+const dailyCanvas = document.getElementById("daily-chart");
+
+// data for daily traffic bar chart
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+    label: '# of Hits',
+    data: [75, 115, 175, 125, 225, 200, 100],
+    backgroundColor: '#7477BF',
+    borderWidth: 1
+    }]
+    };
+    const dailyOptions = {
+    scales: {
+    y: {
+    beginAtZero: true
+    }
+    },
+    plugins: {
+    legend: {
+    display: false
+    }
+    }
+ };
+
+
+ let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+    });
+
+// ********************** doughnut ***************************** 
+
+const mobileCanvas = document.getElementById("mobile-chart");
+
+
+
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+    label: '# of Users',
+    data: [2000, 550, 500],
+    
+    
+    borderWidth: 0,
+    backgroundColor: [
+    '#7477BF',
+    '#78CF82',
+    '#51B6C8'
+    ]
+    }]
+};
+
+
+const mobileOptions = {
+    
+    plugins: {
+    legend: {
+    position: 'right',
+    
+    labels: {
+        
+        padding: 100,   
+            
+        
+        font: {
+            size: 20
+        },
+        
+    boxWidth: 40,
+    fontStyle: 'bold',
+    
+    
+    
+    
+    }
+    }
+    }
+};
+
+
+
+let mobileChart = new Chart(mobileCanvas, {
+        type: 'doughnut',
+        data: mobileData,
+        
+        
+        options: mobileOptions
+});
+
+
+// ********************** Messaging Section ***************************** 
+
+
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener('click', () => {
+    // ensure user and message fields are filled out
+    if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+    } else if (user.value === "" ) {
+    alert("Please fill out user field before sending");
+    } else if (message.value === "" ) {
+    alert("Please fill out message field before sending");
+    } else {
+    alert(`Message successfully sent to: ${user.value}`);
+    }
     });
